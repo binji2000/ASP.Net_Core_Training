65bbd93fe9dec2f15cc3e10e5281b39986a27a22
Bin Ji <bin.ji@wolterskluwer.com>
2016-12-21 14:28:49 -0500

    after lab14 - revert libaries to 1.0.1

---
+++ Lab01_BJi/EComm/EComm.Tests/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EComm.Data;
+using EComm.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace EComm.Tests
+{
+    public class Class1
+    {
+        
+        public Class1()
+        {
+            
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, (2 + 2));
+        }
+        [Fact]
+        public void ProductDetails()
+        {
+            // Arrange
+            var controller = new ProductController(CreateStubContext());
+
+            // Act
+            var result = controller.Detail(2);
+
+            // Assert
+            Assert.IsAssignableFrom<ViewResult>(result);
+            var vr = result as ViewResult;
+            Assert.IsAssignableFrom<Product>(vr.Model);
+            var model = vr.Model as Product;
+            Assert.Equal("Bread", model.ProductName);
+        }
+
+        private ECommContext CreateStubContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ECommContext>();
+            optionsBuilder.UseInMemoryDatabase();
+            var context = new ECommContext(optionsBuilder.Options);
+
+            // Add sample data
+            context.Products.Add(new Product { Id = 1, ProductName = "Milk", UnitPrice = 2.50M });
+            context.Products.Add(new Product { Id = 2, ProductName = "Bread", UnitPrice = 3.25M });
+            context.Products.Add(new Product { Id = 3, ProductName = "Juice", UnitPrice = 5.75M });
+            context.SaveChanges();
+
+            return context;
+        }
+
+    }
+}
